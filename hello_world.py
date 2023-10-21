79d4ea3fa1fa082cf9ba28342155761a90cbef6c
ibrahim qabalah <ibrahimqabala@gmail.com>
2023-10-21 21:53:30 +0300

    assigment

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+print( "hello world" )
+
+name = "Noelle"
+print( "hello",name )	
+print( "hello " + name)	
+name = "42"
+print( "hello",name )	
+print( "hello "+name )
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to eat {} and {}".format(fave_food1,fave_food2)) 
+print( f"I love to eat {fave_food1} and {fave_food2}") 
+print( "I love to eat %s and %s" %(fave_food1,fave_food2) )
+
